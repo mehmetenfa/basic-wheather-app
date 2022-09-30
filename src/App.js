470b36de94d28c5645761b63ffe15cfd6e2434a1
Mehmet Enfa Vakitsayan <mehmet.enfa@gmail.com>
2022-09-30 14:25:22 +0300

    completed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import './App.css'
-import axios from 'axios'
-import { useEffect } from 'react'
-
+import axios from "axios";
+import { useEffect, useState } from "react";
+import "./App.css";
+import City from "./City";
 function App() {
-  const key = 'f6c0a3bef4112a87f526c8319a6182e7'
+  const key = "84a91b51124f2bc14e2104a40efa11c9";
+  const [search, setSearch] = useState("");
+  const [city, setCity] = useState();
   useEffect(() => {
     async function getApi() {
       try {
         const response = await axios.get(
-          `https://api.openweathermap.org/data/2.5/weather?q=London&appid=${key}`
-        )
-        console.log(response)
+          `https://api.openweathermap.org/data/2.5/weather?q=${search}&appid=${key}&units=metric`
+        );
+        console.log(response);
+        setCity(response.data);
       } catch (error) {
-        console.error(error)
+        console.error(error);
       }
     }
-    getApi()
-  })
-  return <div className="App">hello</div>
+    getApi();
+  }, [search]);
+  console.log(search);
+  return (
+    <div className="App">
+      <div>
+        <input
+          onChange={(e) => setSearch(e.target.value)}
+          type="text"
+          placeholder="Placeholder"
+          className=" my-5 px-3 w-[250px] py-3 placeholder-blueGray-300 text-blueGray-600   bg-white rounded text-sm border-0 shadow outline-none focus:outline-none focus:ring "
+        />
+        {city && <City city={city} />}
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
